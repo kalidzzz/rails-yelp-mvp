70119260955bfedebc62c7ad60a944a8fd2745be
zanoune kalid <zanounekalid@hotmail.fr>
2022-05-18 15:38:43 +0200

    exo2

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,30 @@
+class RestaurantsController < ApplicationController
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def create
+    @restaurant = Restaurant.create(restaurant_params)
+
+     @restaurant.save
+
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.create(name: 'mcdo', address: 'cours de lune', phone_number: '088776666', category: 'french')
+Restaurant.create(name: 'LUNE', address: 'cours de mars', phone_number: '088776266', category: 'belgian')
+Restaurant.create(name: 'mars', address: 'cours de saturne', phone_number: '088774666', category: 'japanese')
+Restaurant.create(name: 'saturne', address: 'cours de jupiter', phone_number: '088777666', category: 'italian ')
+Restaurant.create(name: 'jupiter', address: 'cours de neptune', phone_number: '08877666', category: 'french')
